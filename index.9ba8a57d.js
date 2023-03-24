3ea4893fe8b2cf8a4d07a35ea703eb9197fd91f4
Viacheslav009 <114593269+Viacheslav009@users.noreply.github.com>
2023-03-24 09:04:32 +0000

    Deploying to gh-pages from @ Viacheslav009/goit-js-hw-10@7777b7cc0862d8820a168bc2d03dd4f87f38735c 🚀

---
+++ index.9ba8a57d.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".searchbox > input");e.addEventListener("input",(function(){console.log(e.value)}))}();
+//# sourceMappingURL=index.9ba8a57d.js.map
---
+++ index.b3266901.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".searchbox > input");e.addEventListener("input",(()=>{console.log(e.value)}));
+//# sourceMappingURL=index.b3266901.js.map

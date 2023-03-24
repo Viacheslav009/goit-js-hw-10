7777b7cc0862d8820a168bc2d03dd4f87f38735c
Viacheslav009 <mstislavpushkov@gmail.com>
2023-03-24 11:04:14 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const searchbox = document.querySelector('.searchbox > input');
+searchbox.addEventListener('input', () => {
+  console.log(searchbox.value);
+});
